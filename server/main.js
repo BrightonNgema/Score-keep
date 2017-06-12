f074db24eac5f61b685a6ace533232b1ad56b20b
BrightonNgema <brightonnkosana@gmail.com>
2017-06-12 16:39:36 +0200

    add data using form

---
+++ server/main.js
@@ -0,0 +1,10 @@
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+
+Meteor.startup(function(){
+ // Players.insert({
+ //   name: 'Khanyi',
+ //   score: 50
+ // });
+ // console.log(Players.find().fetch());
+});
