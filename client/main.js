1a29c8eb177530cfdac00c506e7bbd1e8dc219c6
BrightonNgema <brightonnkosana@gmail.com>
2017-06-13 10:44:57 +0200

    Score Update

--- client/main.js
+++ client/main.js
@@ -5,13 +5,27 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 
 
-const renderPlayers = function(playersList){
-return playersList.map(function(player){
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+const renderPlayers = (playersList) => {
+return playersList.map((player) => {
+    return (
+        <p key={player._id}>
+          {player.name} has {player.score} point(s).
+          <button onClick={() =>Players.update
+                (player._id, {$inc: {score :-1}})}>-1
+          </button>
+          <button onClick={() =>Players.update
+              (player._id, {$inc: {score :1}})}>+1
+
+          </button>
+          <button onClick={() =>Players.remove(player._id)}>X </button>
+        </p>
+      );
+
   });
 };
 
-const handleSubmit = function (e){
+
+const handleSubmit =(e) =>{
   let playerName = e.target.playerName.value;
     e.preventDefault();
 
@@ -24,23 +38,23 @@ const handleSubmit = function (e){
     }
 };
 
-Meteor.startup(function(){
-  Tracker.autorun(function(){
+Meteor.startup(()=>{
+  Tracker.autorun(()=>{
     let players = Players.find().fetch();
     let title = 'Score Keep';
     let name = 'Brighton';
     let jsx = (
-              <div>
-                <h1>{title}</h1>
-                <p> Hello {name}!</p>
-                <p>Second p</p>
-                {renderPlayers(players)}
-                <form onSubmit={handleSubmit}>
-                  <input type="text" name="playerName" placeholder="Player Name"/>
-                  <button>Add Player</button>
-                </form>
-              </div>
-            );
+          <div>
+              <h1>{title}</h1>
+              <p> Hello {name}!</p>
+              <p>Second p</p>
+              {renderPlayers(players)}
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="playerName" placeholder="Player Name"/>
+              <button>Add Player</button>
+            </form>
+          </div>
+          );
     ReactDOM.render(jsx, document.getElementById('app'));
 
   });
--- server/main.js
+++ server/main.js
@@ -7,4 +7,29 @@ Meteor.startup(function(){
  //   score: 50
  // });
  // console.log(Players.find().fetch());
+
+ // let square = (x) => x * x;
+ // console.log(square(10))
+
+
+ let user = {
+   name : 'KC',
+
+   sayHi (){
+    // console.log(this.name);
+    setTimeout(()=>{
+      console.log(this.name);
+    },1000);
+   }
+ };
+ user.sayHi();
+
+
+ let numbers = [9, 99, 4, 56];
+ let newNUmbers = numbers.map(function(number){
+
+    return number +2;
+ });
+
+ console.log(newNUmbers);
 });
